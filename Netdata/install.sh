d48c76c677c6784c28bb2765aad2e3760a8ec8df
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-09 23:55:24 +0100

    Update install.sh
    edit

--- Netdata/install.sh
+++ Netdata/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl -s https://packagecloud.io/install/repositories/netdata/netdata-repoconfig/script.deb.sh
-sudo bash
+sudo apt update
 sudo apt install netdata -y
 sudo ufw allow 19999/tcp
 sudo vim /etc/netdata/netdata.conf 
