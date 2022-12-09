8246aea17a985811b3197a6dcb708527fe9d9979
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-10 00:27:19 +0100

    FIX

--- NGINX/install.sh
+++ NGINX/install.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo apt-get update
-sudo apt-get install docker-compose-plugin -y
-sudo docker compose version
+sudo apt-get install docker-compose -y
+sudo docker-compose version
 sudo docker-compose up -d
