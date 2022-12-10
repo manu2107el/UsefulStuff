6b77a625a8ded0242ceec87a43ebca03e2c943fb
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-10 15:37:40 +0100

    Som shit idk

--- SRVNETclient/install.sh
+++ SRVNETclient/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 curl -s https://install.zerotier.com | sudo bash
 zerotier-cli join da558169b63ccb91
---
+++ ZTCUI/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install docker.io -y
+$ git clone https://github.com/key-networks/ztncui-aio
+$ docker pull keynetworks/ztncui
+$ docker run -d -p9993:9993/udp -p3443:3443 -p3180:3180 \
+    -v /mydata/ztncui:/opt/key-networks/ztncui/etc \
+    -v /mydata/zt1:/var/lib/zerotier-one \
+    --env-file ./denv \
+    --name ztncui \
+    keynetworks/ztncui
