03b267d9974719774b0044d23963d84b2d3df3d6
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-10 15:45:13 +0100

    Update install.sh

--- ZTCUI/install.sh
+++ ZTCUI/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install docker.io -y
 git clone https://github.com/key-networks/ztncui-aio
-dc ztncui-aio
+cd ztncui-aio
 docker pull keynetworks/ztncui
 docker run -d -p9993:9993/udp -p3443:3443 -p3180:3180 \
     -v /mydata/ztncui:/opt/key-networks/ztncui/etc \
