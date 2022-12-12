a9ed07e85c4260c959d98f5ecdb23a7307d735c3
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-12 18:30:13 +0100

    das

---
+++ HBS/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -s https://install.zerotier.com | sudo bash
+zerotier-cli join da558169b63ccb91
+sudo apt install netdata -y
+sudo ufw allow 19999/tcp
+sudo vim /etc/netdata/netdata.conf 
