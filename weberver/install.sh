14abbf135873754bd814d797030270a72d861e05
Manuel <81967683+manu2107el@users.noreply.github.com>
2022-12-13 22:49:13 +0100

    I think that works (maybe)

--- weberver/install.sh
+++ weberver/install.sh
@@ -17,23 +17,19 @@ sudo a2enmod rewrite
 
 # Install the FTP server
 sudo apt-get install vsftpd -y
-#sudo mv vsftpd.conf /etc/vsftpd.conf
-sudo groupadd ftpusers
+sudo mv vsftpd.conf /etc/vsftpd.conf
 
-# add Apache user to FTP user group
-sudo usermod -a -G ftpusers www-data
-
-# set the Apache root directory as the home directory for FTP users
-sudo sed -i 's/local_root=\/var\/www/local_root=\/var\/www\/html/' /etc/vsftpd.conf
-
-# allow FTP users to write to the Apache root directory
-sudo sed -i 's/write_enable=NO/write_enable=YES/' /etc/vsftpd.conf
 #user
-sudo adduser ftpusers
-#sudo usermod -d /var/www -m ftpusers
-#sudo usermod -a -G www-data ftpusers
-#sudo chgrp -R www-data /var/www
-#sudo chmod -R g+w /var/www
+sudo groupadd ftpusers
+sudo adduser ftpuser
+sudo usermod -d /var -m ftpuser
+sudo usermod -a -G ftpusers ftpuser
+sudo usermod -a -G www-data ftpuser
+sudo chmod -R g+w /var/www
+sudo chgrp -R www-data /var/www   
+sudo find /var/www -type d -exec chmod 2775 {} \;
+sudo find /var/www -type f -exec chmod ug+rw {} \;
+
 # Restart to apply changes
 service vsftpd restart
 service apache2 restart
