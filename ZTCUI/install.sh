3f8f85427b3157cf0d51808f6510d1ff03323f82
Manuel <81967683+manu2107el@users.noreply.github.com>
2023-01-16 13:36:36 +0100

    Update install.sh
    Quick Fix

--- ZTCUI/install.sh
+++ ZTCUI/install.sh
@@ -3,20 +3,17 @@
 #Get zerotier
 curl -s https://install.zerotier.com | sudo bash
 
-#get netdata
-sudo apt install netdata -y
-sudo ufw allow 19999/tcp
-sudo vim /etc/netdata/netdata.conf 
-
 sudo apt-get update
 sudo apt-get -y upgrade
 
 #INSTALL
 sudo apt install docker.io -y
+cd
 git clone https://github.com/key-networks/ztncui-aio
 cd ztncui-aio
-docker pull keynetworks/ztncui
-docker run -d -p9993:9993/udp -p3443:3443 -p3180:3180 \
+sudo docker pull keynetworks/ztncui
+sudo vim denv
+sudo docker run -d -p9993:9993/udp -p3443:3443 -p3180:3180 \
     -v /mydata/ztncui:/opt/key-networks/ztncui/etc \
     -v /mydata/zt1:/var/lib/zerotier-one \
     --env-file ./denv \
